13686aecb1288441613482b3ef89688a530a2bc8
Eric Che <ericche@berkeley.edu>
2023-11-08 02:14:21 -0800

    Add odd logic

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -2,6 +2,10 @@ package Demo;
 
 public class Demo {
     public static void main(String[] args) {
+        String x = "Eric Che";
+        if (x.length() % 2 == 1) {
+            System.out.println("Eric Che is odd");
+        }
         System.out.println("Hello World");
     }
 }
