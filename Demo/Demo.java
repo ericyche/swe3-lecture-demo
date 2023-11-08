d80eec051e8e5eeae1f41a3fdc40a795ca9ade0b
Eric Che <ericche@berkeley.edu>
2023-11-08 02:14:07 -0800

    Add even logic

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -2,6 +2,10 @@ package Demo;
 
 public class Demo {
     public static void main(String[] args) {
+        String x = "Eric Che";
+        if (x.length() % 2 == 0) {
+            System.out.println("Eric Che is Even");
+        }
         System.out.println("Hello World");
     }
 }
