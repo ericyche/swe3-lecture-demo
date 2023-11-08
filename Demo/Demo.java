529fe59ab421decc0c555d6fe302d2f3a9e115b4
Eric Che <ericche@berkeley.edu>
2023-11-08 02:34:08 -0800

    Finish World

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -7,6 +7,9 @@ public class Demo {
         int x = new Random().nextInt();
         // Task 1: If number is even, print "Hello"
         // Task 2: If number is odd, print "World"
+        if (x % 2 == 1) {
+            System.out.println("World");
+        }
         System.out.println("Test");
     }
 }
